2845841e84570a8270dd629ea97cd686ff0b26e0
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-12-06 22:26:54 +0300

    fix: rollback page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 
-import { DefaultLayout } from "../layouts/Default";
+// import { DefaultLayout } from "../layouts/Default";
 
 export default function Home() {
   return (
-    <DefaultLayout>
-      <div className="text-3xl font-bold underline">Preparing!</div>
-    </DefaultLayout>
+    <div className="w-full h-screen flex items-center justify-center text-[60px] font-semibold">
+      Preparing!
+    </div>
   );
 }
