14773141ebe3a896e9d952829ab8ba400e394103
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-12-06 11:29:29 +0300

    init: adds preparing text

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  plugins: [
+    [
+      "postcss-nested",
+      {
+        // Add any options here
+      },
+    ],
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
