219327ebdd3f3473d207b7a263995e8e5b6939cc
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-12-06 22:37:31 +0300

    fix adds theme sync

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center text-[60px] text-gray-900 font-semibold dark:bg-black ">
+    <div className="w-full h-screen flex items-center justify-center text-[60px] bg-white text-gray-900 font-semibold dark:bg-black dark:text-white">
       Preparing!
     </div>
   );
