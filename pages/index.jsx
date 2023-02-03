c9e1e7a5e67346f35b18f588bab8477865668390
Gökhan Öztürk <gokhaanozturk@gmail.com>
2023-02-03 19:23:33 +0300

    added react-spring & created an example

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,59 @@
 import DefaultLayout from '../layouts/Default';
 const img = '@assets/index.jpg';
+import { useSpringRef, animated, useTransition, useSpring } from 'react-spring';
+import { useLayoutEffect, useState } from 'react';
+
+const IMAGES = [
+  'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/154443660/original/734570cec0de955789ff0acd80caad3d582b85e0/create-a-generative-art-piece-for-you.png',
+  'https://images.squarespace-cdn.com/content/v1/5c77350965a707ed1710a1bc/1592330659753-70M66LGEPXFTQ8S716MX/Generative+Art+by+Mark+Stock+-+Gyre+35700.jpg',
+  'https://cdn.pixabay.com/photo/2018/09/04/09/12/generative-art-3653275_1280.jpg',
+];
 
 export default function Home() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const springApi = useSpringRef();
+
+  const transitions = useTransition(activeIndex, {
+    from: {
+      clipPath: 'polygon(0% 0%, 0% 100%, 0% 100%, 0% 0%)',
+    },
+    enter: {
+      clipPath: 'polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)',
+    },
+    leave: {
+      clipPath: 'polygon(100% 0%, 100% 100%, 100% 100%, 100% 0%)',
+    },
+    onRest: (_springs, _ctrl, item) => {
+      if (activeIndex === item) {
+        setActiveIndex(activeIndex === IMAGES.length - 1 ? 0 : activeIndex + 1);
+      }
+    },
+    exitBeforeEnter: true,
+    config: {
+      duration: 4000,
+    },
+    delay: 1000,
+    ref: springApi,
+  });
+
+  const springs = useSpring({
+    from: {
+      strokeDashoffset: 120,
+    },
+    to: {
+      strokeDashoffset: 0,
+    },
+    config: {
+      duration: 11000,
+    },
+    loop: true,
+    ref: springApi,
+  });
+
+  useLayoutEffect(() => {
+    springApi.start();
+  }, [activeIndex]);
+
   return (
     <DefaultLayout title="Gökhan Öztürk">
       <div className="mb-10">
@@ -64,7 +116,11 @@ export default function Home() {
         . */}
       </div>
       <div className="mt-10">
-        <img className="rounded-2xl aspect-video" src="index.jpg" />
+        {transitions((springs, item) => (
+          <animated.div style={springs}>
+            <img src={IMAGES[item]} />
+          </animated.div>
+        ))}
       </div>
     </DefaultLayout>
   );
