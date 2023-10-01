6523ccf057f8c893f623c34af5caf4e1436b4f3b
Gokhan Ozturk <gokhaanozturk@gmail.com>
2023-10-01 19:09:36 +0300

    add blog & bookmarks

--- pages/bookmarks.jsx
+++ pages/bookmarks.jsx
@@ -1,17 +1,11 @@
 import DefaultLayout from '../layouts/Default';
 
-export default function Blog() {
+export default function Bookmarks() {
   return (
     <DefaultLayout>
       <div className="flex flex-col items-start w-full">
         <div className="flex gap-4">
           <span className="text-gray-500">1.10.2023</span>
-          <a
-            href="https://medium.com/@gokhaanozturk/vue-js-ile-özelleştirilebilir-breadcrumb-bileşeni-oluşturma-92a8e94a199e"
-            className="hover:underline"
-          >
-            breadcrumbs
-          </a>
         </div>
       </div>
     </DefaultLayout>
