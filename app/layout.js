7e0c8bb22795b5350d8f133ffbf7c8979662ea45
Gökhan Öztürk <gokhaanozturk@gmail.com>
2023-10-24 10:32:33 +0300

    WIP: seo improvements

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,37 @@
 import { Header } from "../components/Header";
-import { metadata } from '../constants/metadata'
 import { Analytics } from '@vercel/analytics/react';
 
-export const metadataBase = metadata.metadataBase;
+export const metadata = {
+	metadataBase: new URL('https://gokhanozturk.io'),
+	title: {
+		default: 'Gökhan Öztürk – Developer, crafter.',
+		template: '%s | Gökhan Öztürk – Developer, crafter.',
+	},
+	description: `Front-end developer, JavaScript enthusiast.`,
+	openGraph: {
+		title: 'Gökhan Öztürk – Developer, crafter.',
+		description: `Front-end developer, JavaScript enthusiast.`,
+		url: 'https://gokhanozturk.io',
+		site_name: 'Gökhan Öztürk',
+		locale: 'en_US',
+		type: 'website',
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+	twitter: {
+		title: 'Gökhan Öztürk',
+		card: 'summary_large_image',
+	},
+};
 
 export default function RootLayout({
 	// Layouts must accept a children prop.
