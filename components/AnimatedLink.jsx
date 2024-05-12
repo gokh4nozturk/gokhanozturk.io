d4ee41fded7fe99f4fe037befa6475dfd222c263
Gokhan Ozturk <gokhaanozturk@gmail.com>
2024-05-12 15:32:06 +0300

    feat: Update NavItem and AnimatedLink

--- components/AnimatedLink.jsx
+++ components/AnimatedLink.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 export default function AnimatedLink({ children, color = 'black', href = '#' }) {
   const textHoverColor = {
-    black: 'hover:text-gray-800',
+    black: 'hover:text-gray-900 dark:hover:text-gray-200',
     rockads: 'hover:text-[#007CE1]',
     teknasyon: 'hover:text-[#009cfb]',
   }[color]
   const fromColor = {
-    black: 'from-gray-800',
+    black: 'from-gray-900 dark:from-gray-200',
     rockads: 'from-[#007CE1]',
     teknasyon: 'from-[#009cfb]',
   }[color]
   const toColor = {
-    black: 'to-gray-800',
+    black: 'to-gray-900 dark:to-gray-200',
     rockads: 'to-[#007CE1]',
     teknasyon: 'to-[#009cfb]',
   }[color]
