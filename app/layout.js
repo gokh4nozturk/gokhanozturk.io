606a4159f8f7b538b1fb61b25d5aa1d65ccd40cf
Gokhan Ozturk <gokhaanozturk@gmail.com>
2024-05-12 22:46:42 +0300

    chore: Add `SpeedInsight`

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Header } from '../components/Header'
 import '../styles/globals.css'
 
@@ -51,6 +52,7 @@ export default function RootLayout({
 
         <main className="main mt-10">{children}</main>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
