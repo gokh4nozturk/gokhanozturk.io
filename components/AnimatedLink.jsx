42ab2ac10c164db447a23fbe93461a8f0ba7e7a7
Gokhan Ozturk <gokhaanozturk@gmail.com>
2024-05-12 15:48:39 +0300

    feat: Improve hover effect on AnimatedLink

--- components/AnimatedLink.jsx
+++ components/AnimatedLink.jsx
@@ -1,10 +1,11 @@
+import cn from 'classnames'
 import React from 'react'
 
 export default function AnimatedLink({ children, color = 'black', href = '#' }) {
-  const textHoverColor = {
-    black: 'hover:text-gray-900 dark:hover:text-gray-200',
-    rockads: 'hover:text-[#007CE1]',
-    teknasyon: 'hover:text-[#009cfb]',
+  const textColor = {
+    black: 'text-gray-900 dark:text-gray-200',
+    rockads: 'text-[#007CE1]',
+    teknasyon: 'text-[#009cfb]',
   }[color]
   const fromColor = {
     black: 'from-gray-900 dark:from-gray-200',
@@ -19,13 +20,13 @@ export default function AnimatedLink({ children, color = 'black', href = '#' }) 
 
   return (
     <a
-      className={`group ${textHoverColor} transition-all duration-300 ease-in-out`}
+      className={cn('group transition-all duration-300 ease-in-out', `hover:${textColor}`)}
       href={href}
       target="_blank"
     >
       <span className={`
       bg-gradient-to-r ${fromColor} ${toColor} bg-[length:0%_1px] bg-left-bottom bg-no-repeat 
-      transition-all duration-500 ease-out group-hover:bg-[length:100%_1px]`
+      transition-all duration-500 ease-out group-hover:bg-[length:100%_1px] max-sm:bg-[length:100%_1px]`
       }>
         {children}
       </span>
