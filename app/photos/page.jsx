7c10286cc97f0a0416c64c8a04155679ed94656b
Gokhan Ozturk <gokhaanozturk@gmail.com>
2024-11-10 20:44:31 +0300

    feat: add metadata generation for bookmarks, photos, and works pages

--- app/photos/page.jsx
+++ app/photos/page.jsx
@@ -31,3 +31,20 @@ function Photo({ data: photo, index }) {
     </NextLink>
   )
 }
+
+export async function generateMetadata() {
+  const siteUrl = '/photos'
+
+  return {
+    title: 'Photos',
+    description: 'A collection of photos',
+    openGraph: {
+      title: 'Photos',
+      description: 'A collection of photos',
+      url: siteUrl,
+    },
+    alternates: {
+      canonical: siteUrl,
+    },
+  }
+}
