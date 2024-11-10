4912f5f5c5c3bb2b70c1229f89acb20e01c0ee6d
Gokhan Ozturk <gokhaanozturk@gmail.com>
2024-11-10 20:56:28 +0300

    feat: add robots and sitemap generation for SEO optimization

---
+++ app/robots.js
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://gokhanozturk.io/sitemap.xml',
+    host: 'https://gokhanozturk.io',
+  }
+}
