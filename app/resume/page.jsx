616ae0df217095a3bfb143d3eeffe7e4d50afb96
gokhanozturk <gokhaanozturk@gmail.com>
2025-03-07 00:25:08 +0300

    refactor: Enhance MDX components with improved styling and responsiveness

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -4,7 +4,7 @@ import { MDXProvider } from '@mdx-js/react'
 import ResumeContent from './content.mdx'
 import components from '../components/mdx-components'
 
-export default function resumePage() {
+export default function ResumePage() {
   return (
     <MDXProvider components={components}>
       <div className="prose prose-gray dark:prose-invert max-w-none">
