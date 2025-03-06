215fb3f7c75f3bedacdabf8cfd787f65f55bd80c
gokhanozturk <gokhaanozturk@gmail.com>
2025-03-06 15:00:25 +0300

    feat: update not found

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,7 +1,12 @@
+import Image from 'next/image'
+
 export default function NotFound() {
   return (
     <div className="mt-40 flex items-center justify-center">
-      <h1 className="text-4xl font-bold">404 - Not Found</h1>
+      <div className="flex flex-col items-center gap-10">
+        <h1 className="text-6xl font-bold">404 - Not Found</h1>
+        <Image src="/taken.svg" alt="404" width={600} height={600} />
+      </div>
     </div>
   )
 }
