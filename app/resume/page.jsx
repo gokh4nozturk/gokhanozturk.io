6f48b6b62008128a40c755d3574d62b816495cc8
gokhanozturk <gokhaanozturk@gmail.com>
2025-03-06 23:17:44 +0300

    refactor: Rename career page to resume and update related components

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { MDXProvider } from '@mdx-js/react'
-import CareerContent from './content.mdx'
+import ResumeContent from './content.mdx'
 import components from '../components/mdx-components'
 
-export default function CareerPage() {
+export default function resumePage() {
   return (
     <MDXProvider components={components}>
       <div className="prose prose-gray dark:prose-invert max-w-none">
-        <CareerContent />
+        <ResumeContent />
       </div>
     </MDXProvider>
   )
