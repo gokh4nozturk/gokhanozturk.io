90ff3ec1b13e947ff52f445c89b98f23146ce49b
gokhanozturk <gokhaanozturk@gmail.com>
2025-03-12 19:05:03 +0300

    style: Adjust navigation component styling and increase padding in resume layout

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -53,7 +53,7 @@ export default function Navigation() {
         )}
       </AnimatePresence>
       <div className="fixed sm:bottom-10 bottom-6 w-full px-4 z-50 left-1/2 -translate-x-1/2">
-        <nav className="mx-auto flex h-[52px] max-w-fit items-center gap-1 rounded-full border border-[#eaeaea] bg-white/80 px-4 py-2 shadow-[0_2px_4px_rgba(0,0,0,0.02),0_1px_6px_rgba(0,0,0,0.03)] backdrop-blur-md dark:border-neutral-800 dark:bg-neutral-900/80">
+        <nav className="mx-auto flex h-[52px] max-w-fit items-center gap-1 rounded-full border border-[#eaeaea]  px-4 py-2 backdrop-blur-md dark:border-neutral-800 ">
           {navItems.map(({ path, name, icon }) => {
             const isActive = path === pathname;
             const Icon = icon;
--- app/resume/layout.jsx
+++ app/resume/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function ResumeLayout({ children }) {
   return (
-    <article className="prose prose-neutral mx-auto max-w-2xl pb-20 dark:prose-invert">
+    <article className="prose prose-neutral mx-auto max-w-2xl pb-28 dark:prose-invert">
       {children}
     </article>
   );
