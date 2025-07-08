4c5d443146e4dd0d6afefba409e8024486eab1f4
gokhanozturk <gokhaanozturk@gmail.com>
2025-07-08 12:54:16 +0300

    Refactor page component to use single quotes for imports and add AppleHelloEnglishEffect animation. Comment out ASCIIText component for future use.

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,24 @@
-"use client";
+'use client';
 
-import About from "@components/About";
-import ASCIIText from "@components/reactbits/ASCIIText";
-import { cn } from "@lib/utils";
+import About from '@components/About';
+import { AppleHelloEnglishEffect } from '@components/apple-hello-effect';
+import ASCIIText from '@components/reactbits/ASCIIText';
+import { cn } from '@lib/utils';
 
 export default function Home() {
   return (
     <div className="flex h-full flex-col justify-between sm:relative">
       <About />
-      <ASCIIText
+      {/* <ASCIIText
         className={cn(
-          "-z-10",
-          "fixed bottom-0 left-0 h-96 w-full",
-          "sm:top-32 sm:left-16 sm:h-full",
+          '-z-10',
+          'fixed bottom-0 left-0 h-96 w-full',
+          'sm:top-32 sm:left-16 sm:h-full'
         )}
+      /> */}
+
+      <AppleHelloEnglishEffect
+        className={cn('-translate-x-1/2 -translate-y-2/3 fixed top-2/3 left-1/2 sm:h-32')}
       />
     </div>
   );
