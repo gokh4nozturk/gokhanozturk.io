d6725640cd7981a096d89254613ae8f7aea7f0fc
gokhanozturk <gokhaanozturk@gmail.com>
2026-05-04 10:17:22 +0300

    Enable Buy Me a Coffee link in ThankYou component

--- components/ThankYou.jsx
+++ components/ThankYou.jsx
@@ -6,7 +6,6 @@ import Buymeacoffee from "components/icons/buymeacoffee";
 export default function ThankYou({ className }) {
   return (
     <a
-      aria-disabled="true"
       className={cn(
         "-mr-2.5 ml-1 flex items-center",
         "rounded-full no-underline transition",
@@ -14,15 +13,10 @@ export default function ThankYou({ className }) {
         "dark:bg-p3-accent/40 dark:text-p3-text-light",
         className,
         "hover:animate-tada",
-        "cursor-none opacity-60",
       )}
       href="https://www.buymeacoffee.com/gokhanozturk"
-      onClick={(e) => e.preventDefault()}
     >
-      <span
-        className="flex size-10 shrink-0 items-center justify-center rounded-full bg-black/10 sm:size-10"
-        title="Coming soon!!!"
-      >
+      <span className="flex size-10 shrink-0 items-center justify-center rounded-full bg-black/10 sm:size-10">
         <Buymeacoffee />
       </span>
     </a>
